538acc240d4849e8b3c9e4f2e9925c43c8baa158
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2022-10-27 14:51:27 -0500

    fix bugs

--- weighting_field/src/Integrator.cxx
+++ weighting_field/src/Integrator.cxx
@@ -4,6 +4,8 @@
 #include <assert.h>
 #include <numeric>
 
+#include <cmath>
+
 namespace Integrator {
 
   T::data_t convolve(T::data_t signal_t, 
@@ -17,7 +19,7 @@ namespace Integrator {
     assert(trajectory.size() == velocity.size());
     assert(trajectory.size() == charge.size());
     
-    int num_interp_points = 10;
+    int num_interp_points = 100000;
     T::data_t interpolation_inc = 1.0 / num_interp_points;
 
     // interpolate trajectory onto finer grid
@@ -32,7 +34,7 @@ namespace Integrator {
     for(int ind = 0; ind < trajectory.size() - 1; ind++) {      
       for(int step = 0; step < num_interp_points; step++) {
 	
-	T::data_t lambda = (T::data_t)step / num_interp_points;	
+	T::data_t lambda = (T::data_t)(step) / num_interp_points;	
 	T::point_4d_t cur_pt = T::add(T::mul(trajectory[ind], 1 - lambda), T::mul(trajectory[ind + 1], lambda));
 	T::vector_t cur_velocity = T::add(T::mul(velocity[ind], 1 - lambda), T::mul(velocity[ind + 1], lambda));
 	T::data_t cur_charge = charge[ind] * (1 - lambda) + charge[ind + 1] * lambda;
@@ -49,9 +51,9 @@ namespace Integrator {
 
     // after the interpolation, the integrand is piecewise linear
     // to first order, can now simply run trapezoidal integration in between subsequent points
-
+    
     // evaluate the integrand at each (interpolated) point
-    std::vector<T::data_t> integrand;
+    std::vector<T::data_t> integrand_interpolated;
     for(int ind = 0; ind < trajectory_interpolated.size(); ind++) {
 
       T::point_4d_t cur_pt = trajectory_interpolated[ind];
@@ -63,17 +65,17 @@ namespace Integrator {
       weighting_field(signal_t - cur_pt[0], {cur_pt[1], cur_pt[2], cur_pt[3]}, cur_wf);
 
       // compute integrand
-      T::data_t cur_integrand = std::inner_product(cur_wf.begin(), cur_wf.end(), cur_velocity.begin(), 0) / Qw;
-      integrand.push_back(cur_integrand);
+      T::data_t cur_integrand = -cur_charge / Qw * std::inner_product(cur_wf.begin(), cur_wf.end(), cur_velocity.begin(), 0.0);
+      integrand_interpolated.push_back(cur_integrand);
     }
 
     // perform the integration
     T::data_t signal = 0;
-    for(int ind = 0; ind < integrand.size() - 1; ind++) {
-      T::data_t val_l = integrand[ind];
-      T::data_t val_r = integrand[ind + 1];
-      T::data_t time_l = trajectory[ind][0];
-      T::data_t time_r = trajectory[ind + 1][0];
+    for(int ind = 0; ind < integrand_interpolated.size() - 1; ind++) {
+      T::data_t val_l = integrand_interpolated[ind];
+      T::data_t val_r = integrand_interpolated[ind + 1];
+      T::data_t time_l = trajectory_interpolated[ind][0];
+      T::data_t time_r = trajectory_interpolated[ind + 1][0];
 
       signal += (val_l + val_r) * (time_r - time_l) / 2;
     }
--- weighting_field/src/WeightingFieldProvider.cxx
+++ weighting_field/src/WeightingFieldProvider.cxx
@@ -10,11 +10,11 @@ namespace WeightingFieldProvider {
   }
 
   T::data_t dirac_delta(T::data_t x, T::data_t disc_delta) {
-    return std::abs(x) < disc_delta ? 0.5 / disc_delta : 0.0;
+    return std::fabs(x) < disc_delta ? 0.5 / disc_delta : 0.0;
   }
 
   T::data_t dirac_delta_prime(T::data_t x, T::data_t disc_delta) {
-    return std::abs(x) < disc_delta ? std::copysign(1.0, x) / std::pow(disc_delta, 2) : 0.0;
+    return std::fabs(x) < disc_delta ? std::copysign(1.0, x) / std::pow(disc_delta, 2) : 0.0;
   }
 
   void getElectricDipoleWeightingField(const T::data_t eval_t, const T::point_3d_t& eval_point, T::field_t& weighting_field, 
@@ -36,21 +36,21 @@ namespace WeightingFieldProvider {
     T::data_t r = std::sqrt(std::pow(x, 2) + std::pow(y, 2) + std::pow(z, 2));
     T::data_t cos_theta = z / r;
     T::data_t sin_theta = std::sqrt(1.0 - std::pow(cos_theta, 2));
-    T::data_t cos_phi = x / r_xy;
-    T::data_t sin_phi = y / r_xy;
+    T::data_t cos_phi = r_xy != 0 ? x / r_xy : 0;
+    T::data_t sin_phi = r_xy != 0 ? y / r_xy : 0;
 
     T::data_t t_prop = r * n / c;
     T::data_t t_del = t - t_prop;
 
     T::data_t E_theta = -Qw * ds / (eps0 * 4 * M_PI) * sin_theta / std::pow(r, 3) * (
-										  theta(t_del)
-										  + t_prop * dirac_delta(t_del, disc_delta)
-										  + std::pow(t_prop, 2) * dirac_delta_prime(t_prop, disc_delta)
-										  );
+										     theta(t_del)
+										     + t_prop * dirac_delta(t_del, disc_delta)
+										     + std::pow(t_prop, 2) * dirac_delta_prime(t_prop, disc_delta)
+										     );
     T::data_t E_r = -2.0 * Qw * ds / (eps0 * 4 * M_PI) * cos_theta / std::pow(r, 3) * (
-										    theta(t_del)
-										    + t_prop * dirac_delta(t_del, disc_delta)
-										    );
+										       theta(t_del)
+										       + t_prop * dirac_delta(t_del, disc_delta)
+										       );
     
     T::data_t E_x = cos_phi * (E_theta * cos_theta + E_r * sin_theta);
     T::data_t E_y = sin_phi * (E_theta * cos_theta + E_r * sin_theta);
